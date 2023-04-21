cea58080773cf79a4a322081ae272f2ec6eda8a7
eltonk2k <elieltonpb1@gmail.com>
2023-04-20 21:10:59 -0300

    Criando a classe ProcessosBean

--- src/main/java/com/ernestoborges/controller/ProcessosBean.java
+++ src/main/java/com/ernestoborges/controller/ProcessosBean.java
@@ -2,25 +2,32 @@ package com.ernestoborges.controller;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.ernestoborges.model.Processos;
+import com.ernestoborges.service.CadastroProcessosService;
+
 @Named
-@RequestScoped
+@ViewScoped
 public class ProcessosBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private Processos processos;
 	
-	private static Integer NUMERO = 0;
+	@Inject
+	private CadastroProcessosService cadastroProcessosService;
 	
 	
-	
-	public ProcessosBean() {
-		NUMERO++;
+	public void salvar() {
+		cadastroProcessosService.salvar(processos);
 	}
 	
-	public Integer getNumero() {
-		return NUMERO;
+	
+	public Processos getProcessos() {
+		return processos;
 	}
 	
 
